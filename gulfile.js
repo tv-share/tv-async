7cdb191aed524468ebbd3e014a7c91df35eb9065
Thiago Loddi <thiago@thevelops.com>
2018-01-30 17:16:19 -0200

    [master] added gulp and .npmignore

---
+++ gulfile.js
@@ -0,0 +1,16 @@
+var gulp = require('gulp');
+var babel = require('gulp-babel');
+var clean = require('gulp-clean-dest');
+var pump = require('pump');
+
+gulp.task('default', function(cb) {
+
+    const tasks = [
+      gulp.src('src/**/*.js'),
+      clean('dist'),
+      babel(),
+      gulp.dest('dist')
+    ];
+  
+    pump(tasks, cb);
+});
--- src/lib/parallel.js
+++ src/lib/parallel.js
@@ -1,3 +1,16 @@
-export function parallel() {
-    console.log("paralelo tiu!");
+import async from "async";
+
+export function parallel(methods) {
+    const wrapper = {};
+
+    for(let k in methods) {
+        wrapper[k] = async cb => {
+            const reponse = await methods[key]();
+            cb(null, response);
+        }
+    }
+
+    return new Promise((resolve, reject) => {
+        async.parallel(wrapper, (err, result) => resolve(result));
+    });
 }
